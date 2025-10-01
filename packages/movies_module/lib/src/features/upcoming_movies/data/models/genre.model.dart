5863d34fd1e02c1c3d43a0e33d5b448d04303ea4
Alejandra Murillo <amurillo@bancosol.com.bo>
2025-09-30 21:38:14 -0400

    adding models and entities classes

---
+++ packages/movies_module/lib/src/features/upcoming_movies/data/models/genre.model.dart
@@ -0,0 +1,15 @@
+import '../../domain/entities/genre.entity.dart';
+
+class GenreModel extends GenreEntity {
+  const GenreModel({
+    required super.id,
+    required super.name,
+  });
+
+  factory GenreModel.fromJson(Map<String, dynamic> json) {
+    return GenreModel(
+      id: json['id'] as int,
+      name: json['name'] as String,
+    );
+  }
+}
