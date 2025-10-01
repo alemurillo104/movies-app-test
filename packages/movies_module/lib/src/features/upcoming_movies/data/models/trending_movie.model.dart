b3c105a49326e3778121e2483b39923d0f47abf6
Alejandra Murillo <amurillo@bancosol.com.bo>
2025-09-30 21:22:28 -0400

    adding models and entities classes

---
+++ packages/movies_module/lib/src/features/upcoming_movies/data/models/trending_movie.model.dart
@@ -0,0 +1,41 @@
+import '../../domain/entities/trending_movie.entity.dart';
+
+class TrendingMovieModel extends TrendingMovieEntity {
+  TrendingMovieModel({
+    required super.id,
+    required super.title,
+    required super.overview,
+    required super.popularity,
+    required super.voteAverage,
+    required super.releaseDate,
+    required super.genreIds,
+    required super.adult,
+    required super.mediaType,
+    required super.originalTitle,
+    required super.originalLanguage,
+    super.posterPath,
+    super.backdropPath,
+    required super.video,
+    required super.voteCount,
+  });
+
+  factory TrendingMovieModel.fromJson(Map<String, dynamic> json) {
+    return TrendingMovieModel(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      overview: json['overview'] as String,
+      popularity: (json['popularity'] as num).toDouble(),
+      voteAverage: (json['vote_average'] as num).toDouble(),
+      releaseDate: json['release_date'] as String,
+      genreIds: List<int>.from(json['genre_ids'] as List),
+      adult: json['adult'] as bool,
+      mediaType: json['media_type'] as String,
+      originalTitle: json['original_title'] as String,
+      originalLanguage: json['original_language'] as String,
+      posterPath: json['poster_path'] as String?,
+      backdropPath: json['backdrop_path'] as String?,
+      video: json['video'] as bool,
+      voteCount: json['vote_count'] as int,
+    );
+  }
+}
---
+++ packages/movies_module/lib/src/features/upcoming_movies/domain/entities/trending_movie.entity.dart
@@ -0,0 +1,35 @@
+class TrendingMovieEntity {
+  final int id;
+  final String title;
+  final String overview;
+  final double popularity;
+  final double voteAverage;
+  final String releaseDate;
+  final List<int> genreIds;
+  final bool adult;
+  final String mediaType;
+  final String originalTitle;
+  final String originalLanguage;
+  final String? posterPath;
+  final String? backdropPath;
+  final bool video;
+  final int voteCount;
+
+  const TrendingMovieEntity({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.popularity,
+    required this.voteAverage,
+    required this.releaseDate,
+    required this.genreIds,
+    required this.adult,
+    required this.mediaType,
+    required this.originalTitle,
+    required this.originalLanguage,
+    this.posterPath,
+    this.backdropPath,
+    required this.video,
+    required this.voteCount,
+  });
+}
