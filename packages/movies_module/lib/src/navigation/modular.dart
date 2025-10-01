90b813798396512d2be3a2e81078111fa296c5e6
Alejandra Murillo <amurillo@bancosol.com.bo>
2025-09-30 21:17:29 -0400

    logic classes injected in movies modular module class

--- packages/movies_module/lib/src/navigation/modular.dart
+++ packages/movies_module/lib/src/navigation/modular.dart
@@ -1,9 +1,21 @@
 import 'package:common_dependency_module/common_dependency_module.dart';
 import '../features/home/presentation/pages/home.page.dart';
+import '../features/upcoming_movies/data/datasources/remote.datasource.dart';
+import '../features/upcoming_movies/domain/repositories/movies.repository.dart';
+import '../features/upcoming_movies/data/repositories/movies_impl.repository.dart';
+import '../features/upcoming_movies/domain/usecases/get_upcoming_movies.usecase.dart';
+import '../features/upcoming_movies/presentation/blocs/bloc/upcoming_movies_bloc.dart';
 
 class MoviesModularModule extends Module {
+  static T injectorBloc<T extends Bloc>() => Modular.get<T>();
+
   @override
-  void binds(Injector i) {}
+  void binds(Injector i) {
+    i.add(RemoteMoviesDataSource.new);
+    i.add<MoviesRepository>(MoviesImplRepository.new);
+    i.add(GetUpcomingMoviesUseCase.new);
+    i.add(UpcomingMoviesBloc.new);
+  }
 
   @override
   void routes(r) {
